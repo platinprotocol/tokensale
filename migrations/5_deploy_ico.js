4509eaa5e7dd3dab3411ef2963150d4e0dd244e8
Anatolii Kucheruk <anatolii@platin.io>
2018-09-07 14:36:57 +0300

    Changed dates to actual in migrations.

--- migrations/5_deploy_ico.js
+++ migrations/5_deploy_ico.js
@@ -9,8 +9,8 @@ module.exports =  function(deployer, network, accounts) {
     rate = 1000;
 
     wallet = accounts[0]; // TODO change to the real one
-    openingTime = timestamp.fromDate('2018-09-01 12:00:00'); // TODO change to the real one
-    closingTime = timestamp.fromDate('2018-10-01 12:00:00'); // TODO change to the real one
+    openingTime = timestamp.fromDate('2018-10-28 12:00:00'); // TODO change to the real one
+    closingTime = timestamp.fromDate('2018-12-01 10:00:00'); // TODO change to the real one
 
     deployer.deploy(
         PlatinICO,                       
